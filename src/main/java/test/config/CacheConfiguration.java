44c7681844c2c9f2880d1795ad0dd843510dbeb1
coronelmatiasn <coronelmatiasn@gmail.com>
2020-08-23 17:11:38 -0300

    Auto generated code

--- src/main/java/test/config/CacheConfiguration.java
+++ src/main/java/test/config/CacheConfiguration.java
@@ -48,6 +48,8 @@ public class CacheConfiguration {
             createCache(cm, test.domain.User.class.getName());
             createCache(cm, test.domain.Authority.class.getName());
             createCache(cm, test.domain.User.class.getName() + ".authorities");
+            createCache(cm, test.domain.Product.class.getName());
+            createCache(cm, test.domain.ProductBucket.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
